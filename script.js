1aa25102d1e72f2c3be2aabc369b917a1246217a
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-08 15:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	  event.preventDefault();
+
+  // Access the form
+  const form = document.getElementById("form1");
+
+  const firstName = form.fname.value.trim();
+  const lastName = form.lname.value.trim();
+
+  alert(firstName + " " + lastName);
 
 }
